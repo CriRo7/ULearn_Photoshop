f2b8ea3c47b370410c47d94cae7b2f87cf4a083a
Алексей Павлов <ajiexa8@gmail.com>
2019-05-19 17:15:03 +0300

    task 2

--- Data/Pixel.cs
+++ Data/Pixel.cs
@@ -7,9 +7,42 @@ namespace MyPhotoshop.Data
 {
     public class Pixel
     {
-        public double R { get; set; }
-        public double G { get; set; }
-        public double B { get; set; }
+        private double _r;
+        public double R
+        {
+            get { return _r; }
+            set
+            {                
+                _r = CheckValue(value);
+            }   
+        }
+        private double _g;
+        public double G
+        {
+            get { return _g; }
+            set
+            {
+
+                _g = CheckValue(value);
+            }
+        }
+        private double _b;
+        public double B
+        {
+            get { return _b; }
+            set
+            {
+                
+                _b = CheckValue(value);
+            }
+        }
+
+        public double CheckValue(double val)
+        {
+            if (val < 0 || val > 1)
+                throw new ArgumentException();
+            return val;
+        }
 
         public double this[int index] {
             get
