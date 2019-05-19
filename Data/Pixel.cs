d9c9cb35368ee8615f05cac66278352e2f21690c
Алексей Павлов <ajiexa8@gmail.com>
2019-05-19 16:43:55 +0300

    init

---
+++ Data/Pixel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyPhotoshop.Data
+{
+    public class Pixel
+    {
+        public double R { get; set; }
+        public double G { get; set; }
+        public double B { get; set; }
+
+        public double this[int index] {
+            get
+            {
+                switch (index)
+                {
+                    case 0:
+                        return this.R;
+                    case 1:
+                        return this.G;
+                    case 2:
+                        return this.B;
+                    default:
+                        return 0;
+                }
+            }
+            set
+            {
+                switch (index)
+                {
+                    case 0:
+                        this.R = value;
+                        break;
+                    case 1:
+                        this.G = value;
+                        break;
+                    case 2:
+                        this.B = value;
+                        break;
+                    default:
+                        break;
+                }                   
+            }
+        }
+
+    }
+}
